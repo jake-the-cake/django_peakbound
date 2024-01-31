2540d3b8c69d848cdc27b715ef16d07c374dfa7a
Jakey Wakey <askjake331@gmail.com>
2024-01-31 05:14:54 -0500

    modeling

--- flamingo/visitor/apps.py
+++ flamingo/visitor/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class VisitorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'visitor'
+    verbose_name = 'Visitor Log'
--- flamingo/visitor/models.py
+++ flamingo/visitor/models.py
@@ -1,32 +1,78 @@
 from django.db import models
 from _utils.codes import createCharacterCode
 
-class Visitor(models.Model):
-	
-	userCode = models.CharField(
-		max_length = 16,
-		default = createCharacterCode()
-	)
-	
+###
+'''
+	SESSION MODEL:
+	Collect the ip and timestamp each time a new session occurs. A session
+	should end when the browser is closed.
+'''
+class Session(models.Model):
 	ip = models.CharField(
 		max_length = 15,
 		default = '000.000.000.000'
 	)
-	
-	created = models.DateTimeField(
-		auto_now_add = True
+	timestamp = models.DateTimeField( auto_now_add = True )	
+
+###
+'''
+	VISITOR MODEL:
+	Create a cookie in the browser to assign a visitor code. This code will be
+	used to track the number of sessions, as well as the total number of pages
+	that have been visited.
+'''
+class Visitor(models.Model):
+	visitorCode = models.CharField(
+		max_length = 16,
+		default = createCharacterCode(),
+		unique = True
 	)
-	
-	# account = models.ForeignKey(
-	# 	'User',
-	# 	on_delete = models.PROTECT,
-	# 	null = True,
-	# 	default = None
-	# )
-	
-	visits = models.IntegerField(
+	sessions = models.ManyToManyField(
+		Session,
+		on_delete = models.PROTECT,
+	)
+	sessionCount = models.IntegerField(
+		default = 1
+	)
+	pageCount = models.IntegerField(
 		default = 1
 	)
 	
 	def __str__(self):
-		return self.userCode
+		return self.visitorCode
+
+###
+'''
+	USER MODEL:
+	A user profile is generated whenever a transaction occurs
+'''
+class User(models.Model):
+
+	def __str__(self):
+		return self.customerAccount.customerCode
+
+###
+'''
+	CUSTOMER MODEL:
+	When a transaction occurs with no user logged in, the information collected
+	during the transaction will be used to create a customer account (tied to their
+	email address) with a unique customer code, as well as a user profile that
+	contains more information.
+'''
+class Customer(models.Model):
+	customerCode = models.CharField(
+		max_length = 16,
+		default = createCharacterCode(),
+		unique = True,
+	)
+	visitorAccounts = models.ManyToManyField(
+		Visitor,
+		on_delete = models.PROTECT,
+	)
+	email = models.EmailField()
+	created = models.DateTimeField( auto_now_add = True )
+	userProfile = models.ForeignKey(
+		User,
+		on_delete = models.PROTECT,
+		null = False
+	)
