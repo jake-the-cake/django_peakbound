356972e4047258069ee9f4e1fa77d5ae4c8444cd
Jakey Wakey <askjake331@gmail.com>
2024-02-05 20:04:11 -0500

    xome changes

--- flamingo/flamingo/views.py
+++ flamingo/flamingo/views.py
@@ -3,9 +3,7 @@ from django.shortcuts import render
 
 def homepage(request):
 	return render(request, 'home.html')
-	# return HttpResponse('Look at me.')
 
 
 def about(request):
 	return render(request, 'about.html')
-	# return HttpResponse('About face!')
