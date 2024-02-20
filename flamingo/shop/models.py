faa335cf3cebaf88875832fd51c624b08f2b44ce
jake-the-cake <askjake331@gmail.com>
2024-02-20 12:54:11 -0500

    models

--- flamingo/shop/models.py
+++ flamingo/shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from visitor.models import User
 
 # Create your models here.
 class Item(models.Model):
@@ -12,3 +13,26 @@ class Item(models.Model):
 	stock = models.IntegerField(default=1)
 	description = models.TextField(default='description', max_length=500)
 	sku = models.CharField(max_length=24)
+
+class Promo(models.Model):
+	code = models.CharField(max_length=12)
+	description = models.TextField(default='Discount')
+	discount_type = models.CharField(max_length=12, default='percent')
+	discount_value = models.IntegerField(default=0)
+	applicable_items = models.ManyToManyField(Item)
+	can_combine = models.BooleanField(default=False)
+
+class Order(models.Model):
+	order_code = models.CharField(max_length=24)
+	customer = models.ForeignKey(User, on_delete=models.CASCADE)
+	status = models.CharField(max_length=10)
+	total = models.FloatField(default=0)
+	promos = models.ManyToManyField(Promo)
+
+class OrderLine(models.Model):
+	order = models.ForeignKey(Order, on_delete=models.CASCADE)
+	item = models.ForeignKey(Item, on_delete=models.CASCADE)
+	quantity = models.IntegerField(default=1)
+	price = models.FloatField(default=0)
+	promo_price = models.FloatField(default=0)
+	promo_code = models.CharField(max_length=12)
