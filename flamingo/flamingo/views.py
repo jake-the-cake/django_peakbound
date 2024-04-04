66285d8971e2ea44f2e9b6aea1faed613f0f759c
jake-the-cake <askjake331@gmail.com>
2024-04-04 12:30:54 -0400

    logo

--- flamingo/flamingo/views.py
+++ flamingo/flamingo/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 
 def homepage(request):
-	return render(request, 'home.html')
+	return render(request, 'home.html', { 'logo': 'peakbound' })
 
 
 def about(request):
