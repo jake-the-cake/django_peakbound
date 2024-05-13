c064c9f8bd74b4b7b98791562730063ab5b53deb
jake-the-cake <askjake331@gmail.com>
2024-05-13 12:53:56 -0400

    footer work

--- flamingo/_config/links.py
+++ flamingo/_config/links.py
@@ -7,12 +7,12 @@ pages = {
 }
 
 def create_slug(string):
-  return str(string).lower().split(' ')
+  return '-'.join(str(string).lower().split(' '))
 
 
 def link_info(page='peakbound', title=None, url=None):
   if not url:
-    url = '-'.join(create_slug(title))
+    url = create_slug(title)
   return {
     'title': title.upper(),
     'url': path.join(pages[page], url)
